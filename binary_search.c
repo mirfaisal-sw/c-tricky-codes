2c33fb924c06afa9e2d4608227f2e8808a9293e4
MIR FAISAL <80507027+mirfaisal-sw@users.noreply.github.com>
2021-09-05 13:40:21 +0530

    Simple Binary Search

---
+++ binary_search.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int A[] = {11,10,5,32,9,6,5,43,2,76};
+
+int binarySearch(int l,int h,int key)
+{
+    int mid;
+    
+    while(l <= h)
+    {
+        mid = (l + h)/2;
+    
+        if(key == A[mid])
+            return mid;
+        else if(key < A[mid])
+            h = A[mid -1];
+        else
+            l = A[mid + 1];
+    }
+    
+    return -1;
+}
+
+int main()
+{
+    printf("Index of key 43 is - %d\n",binarySearch(0,9,43));
+
+    return 0;
+}
+
+/*************************************
+Output - 
+Index of key 43 is - 7
+**************************************/
