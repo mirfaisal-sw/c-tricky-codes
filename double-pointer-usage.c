f0c5d2a563215649b35d971453d4924d671e60fe
MIR FAISAL <80507027+mirfaisal-sw@users.noreply.github.com>
2021-09-09 22:53:26 +0530

    double-pointer-usage.c

---
+++ double-pointer-usage.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+void fillBuffer(char **ptr)
+{
+    strcpy(*ptr,"Hello world");
+}
+
+int main()
+{
+    char buf[100];
+    char *ptr = buf;
+    
+    fillBuffer(&ptr);
+    
+    printf("buf - %s \n",buf);
+    getchar();
+
+    return 0;
+}
