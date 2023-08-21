ee4c2c035304bedeb61e248f8f68fca2f3f83f6f
MIR FAISAL <80507027+mirfaisal-sw@users.noreply.github.com>
2023-08-21 08:56:45 +0530

    Update double_pointer_usage_4.c

--- double-pointer/double_pointer_usage_4.c
+++ double-pointer/double_pointer_usage_4.c
@@ -1,26 +1,28 @@
 
 #include<stdio.h>
-void function(char**);
+
+void function(char **ptr)
+{
+	char *ptr1;
+  	ptr += sizeof(int);
+	ptr1 = ptr[-2];
+	printf("%s\n", ptr1);
+}
+
 int main()
 {
 	char *arr[] = { "ant", "bat", "cat", "dog", "egg", "fly" };
 	function(arr);
 	return 0;
 }
-void function(char **ptr)
-{
-	char *ptr1;
-  ptr += sizeof(int);
-	ptr1 = ptr[-2];
-	printf("%s\n", ptr1);
-}
 
 Output: - cat
+-------
 
 Explanation: - 
+---------------
 sizeof(int) = 4
 ptr = ptr + 4, will point to "egg".
-
 Hence, ptr[-2] will point to "cat".
   
   
