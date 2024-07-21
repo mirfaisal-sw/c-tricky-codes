7ea193a3c0d99b52f5584de3c2ca5ad81cfa7fad
MIR FAISAL <80507027+mirfaisal-sw@users.noreply.github.com>
2024-07-21 06:50:30 +0530

    Create array_of_string_1.c

---
+++ array-of-string/array_of_string_1.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main()
+{
+    char arr[3][5] = {"Ram", "Mohan", "Shyam"};
+    int i;
+    
+    for(i = 0; i<3; i++)
+    {
+      printf("%s\n", arr[i]);
+    }
+
+    return 0;
+}
+
+O/p - Ram
+      Mohan
+      Shyam
+
+
+  
+
+ 
