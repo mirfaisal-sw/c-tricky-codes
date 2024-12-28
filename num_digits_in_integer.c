e30ea1ab60df9f8e7f23b24ba0bc6e666d27c2eb
MIR FAISAL <80507027+mirfaisal-sw@users.noreply.github.com>
2024-12-28 22:32:01 +0530

    Create num_digits_in_integer.c

---
+++ num_digits_in_integer.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <string.h>
+
+/* Find number of digits in an integer */
+int digits_in_integer(int num)
+{
+    int count = 0;
+  
+    while(num > 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+    int x = 75859;
+    int y = 768;
+    
+    printf("Num of digits in %d is: %d\n", x, digits_in_integer(x));
+    printf("Num of digits in %d is: %d\n", y, digits_in_integer(y));
+    
+    return 0;
+}
+
+/*
+Output: -
+Num of digits in 75859 is: 5
+Num of digits in 768 is: 3
+*/
