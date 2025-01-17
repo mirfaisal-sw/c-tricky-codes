677da5e1b701cdbf8abad6bc71435d92f5d761e0
MIR FAISAL <80507027+mirfaisal-sw@users.noreply.github.com>
2025-01-17 18:12:14 +0530

    Update max_sum_of_subarray.c

--- leetcode_problems/max_sum_of_subarray.c
+++ leetcode_problems/max_sum_of_subarray.c
@@ -19,32 +19,24 @@ There is no subarray of size 3 as size of whole array is 2.
 #include <math.h>
 #include <stdio.h>
 
-// Find maximum between two numbers.
-int max(int num1, int num2)
-{
-    return (num1 > num2) ? num1 : num2;
-}
-
-// Returns maximum sum in a subarray of size k.
 int maxSum(int arr[], int n, int k)
 {
-    // Initialize result
     int max_sum = INT_MIN;
-
-    // Consider all blocks starting with i.
-    for (int i = 0; i < n - k + 1; i++) {
-        int current_sum = 0;
+    int cur_sum;
+    
+    for (int i = 0; i < n - k + 1; i++) 
+    {
+        cur_sum = 0;
         for (int j = 0; j < k; j++)
-            current_sum = current_sum + arr[i + j];
+            cur_sum = cur_sum + arr[i + j];
 
-        // Update result if required.
-        max_sum = max(current_sum, max_sum);
+        if(max_sum < cur_sum)
+            max_sum = cur_sum;
     }
 
     return max_sum;
 }
 
-// Driver code
 int main()
 {
     int arr[] = { 1, 4, 2, 10, 2, 3, 1, 0, 20 };
